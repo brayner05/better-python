c15b523ac2b3683eb5a83c46872efbfe4ce210a1
Brayden Rayner <braydenrayner@outlook.com>
2025-04-29 14:26:16 -0300

    [BROKEN] Refactored AstNode structs and enums to a submodule of parser, and fixed Unary and Binary operations.

---
+++ src/parser/ast.rs
@@ -0,0 +1,141 @@
+use core::fmt;
+use std::rc::Rc;
+
+use crate::lexer::{PieToken, TokenType};
+
+#[derive(Debug)]
+pub enum AstNode {
+    UnaryOperation(UnaryOperation),
+    BinaryOperation(BinaryOperation),
+
+    IntegerLiteral(i64), FloatLiteral(f64), BooleanLiteral(bool),
+    StringLiteral(String), Identifier(String),
+
+    LambdaFunction { params: Vec<Rc<AstNode>>, body: Vec<Rc<AstNode>> },
+    FunctionDefinition(FunctionDefinition)
+}
+
+
+impl fmt::Display for AstNode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            AstNode::UnaryOperation(op) => write!(f, "{}", op),
+
+            AstNode::BinaryOperation(op) => write!(f, "{}", op),
+
+            AstNode::FunctionDefinition(func) => write!(f, "{}", func),
+
+            _ => write!(f, "{:?}", self)
+        }
+    }
+}
+
+
+#[derive(Debug)]
+pub struct UnaryOperation {
+    pub operator: UnaryOperator,
+    pub operand: Rc<AstNode>
+}
+
+
+impl fmt::Display for UnaryOperation {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({} {})", self.operator, self.operand)
+    }
+}
+
+
+#[derive(Debug)]
+pub struct BinaryOperation {
+    pub operator: BinaryOperator,
+    pub left_child: Rc<AstNode>,
+    pub right_child: Rc<AstNode>
+}
+
+
+impl fmt::Display for BinaryOperation {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({} {} {})", self.operator, self.left_child, self.right_child)
+    }
+}
+
+
+pub enum Literal {
+    Integer(i64), Float(f64), Boolean(bool),
+    String(String), Identifier(String)
+}
+
+
+#[derive(Debug)]
+pub struct FunctionDefinition {
+    pub name: String,
+    pub return_type: TokenType,
+    pub param_list: Vec<Rc<AstNode>>,
+    pub body: Vec<Rc<AstNode>>
+}
+
+
+impl fmt::Display for FunctionDefinition {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "(Def {} ({:?}) ({:?})", self.name, self.param_list, self.body)
+    }
+}
+
+
+#[derive(Debug, Clone)]
+pub enum UnaryOperator {
+    Minus, LogicalNot
+}
+
+
+impl fmt::Display for UnaryOperator {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+
+#[derive(Debug, Clone)]
+pub enum BinaryOperator {
+    Plus, Minus, Asterisk, Slash,
+    PlusEqual, MinusEqual, AsteriskEqual, SlashEqual,
+    Modulus, ModulusEqual,
+    Equal, EqualEqual, BangEqual, Less, Greater,
+    LessEqual, GreaterEqual,
+    Or, And
+}
+
+
+impl BinaryOperator {
+    pub fn from(token: &PieToken) -> Option<Self> {
+        match token.type_ {
+            TokenType::Plus => Some(Self::Plus),
+            TokenType::PlusEqual => Some(Self::PlusEqual),
+            TokenType::Minus => Some(Self::Minus),
+            TokenType::MinusEqual => Some(Self::MinusEqual),
+            TokenType::Asterisk => Some(Self::Asterisk),
+            TokenType::AsteriskEqual => Some(Self::AsteriskEqual),
+            TokenType::Slash => Some(Self::Slash),
+            TokenType::SlashEqual => Some(Self::SlashEqual),
+            TokenType::Modulus => Some(Self::Modulus),
+            TokenType::ModulusEqual => Some(Self::ModulusEqual),
+            TokenType::Equal => Some(Self::Equal),
+            TokenType::EqualEqual => Some(Self::EqualEqual),
+            TokenType::BangEqual => Some(Self::BangEqual),
+            TokenType::And => Some(Self::And),
+            TokenType::Or => Some(Self::Or),
+            TokenType::Less => Some(Self::Less),
+            TokenType::LessEqual => Some(Self::LessEqual),
+            TokenType::Greater => Some(Self::Greater),
+            TokenType::GreaterEqual => Some(Self::GreaterEqual),
+            _ => None
+        }
+    }
+}
+
+
+impl fmt::Display for BinaryOperator {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
